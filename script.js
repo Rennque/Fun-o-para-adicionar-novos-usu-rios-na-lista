6829b5e5502dd44d83fb2c216831013309050045
Henrique Fernandes <47489722+Rennque@users.noreply.github.com>
2023-03-03 23:28:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function novosNomes(sub) {
+  const nomes = ["Pedro", "José", "Aderbal", "Danilo", "Luisa", "Vitoria"];
+  console.log(nomes);
+}
+/* let novaLista = ["Pedro", "José", "Aderbal", "Danilo", "Luisa", "Vitoria"];
+  try {
+    sub.forEach((subs) => {
+      if (typeof subs !== "string") {
+        console.log(`So pode ser adicionado nomes`);
+      } else if (novaLista.includes(subs)) {
+        console.log(`Desculpe mais esse nome ja existe`);
+      } else {
+        novaLista.push(subs);
+      }
+    });
+  } catch (error) {
+    console.error(`Ocorreu um erro`);
+  }
+  console.log(novaLista);
+}
+
+novosNomes("Nicolas", "Cintia", "Henrique"); */
